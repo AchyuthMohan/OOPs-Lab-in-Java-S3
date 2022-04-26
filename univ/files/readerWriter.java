aeaf3fabda502044d192d219d1e242dd8a0be9eb
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-04-26 12:56:03 +0530

    updates

--- univ/files/readerWriter.java
+++ univ/files/readerWriter.java
@@ -1,24 +1,13 @@
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Reader;
-
-public class readerWriter {
+import java.io.*;
+class readerWriter{
     public static void main(String[] args) {
         Reader fis=null;
-        FileWriter fout=null;
         try{
-            fis=new FileReader("file.txt");
-            fout=new FileWriter("fileOut.txt");
-            int  ch;
-            String s=new String();
-            s="";
+            fis=new FileReader("abc.txt");
+            int ch;
             while((ch=fis.read())!=-1){
-                // System.out.print((char)ch);
-                char item=(char)ch;
-                s+=item;
+                System.out.print((char)ch);
             }
-            System.out.println(s);
-            fout.write(s);
         }
         catch(Exception e){
             System.out.println(e);
